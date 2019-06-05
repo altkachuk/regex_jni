9e22a17574212fe38ae43c23c207b3162bdcb7d1
Andrey Tkachuk <andreyltkachuk@gmail.com>
2019-06-05 21:40:45 +0300

    V1

--- app/src/main/cpp/jni_interface.h
+++ app/src/main/cpp/jni_interface.h
@@ -13,10 +13,31 @@
 extern "C" {
 #endif
 
+static void InitApplication(JNIEnv* env, jobject instance);
+static void SetListener(JNIEnv* env, jobject instance, jobject nativeCallLitener);
+static jboolean SetFilter(JNIEnv *env, jobject instance, jstring jfilter);
+static jboolean AddSourceBlock(JNIEnv *env, jobject instance, jstring jblock);
+static void StartParsing(JNIEnv *env, jobject instance);
+static jint TestParsing(JNIEnv *env, jobject instance);
+
+static JNINativeMethod methods[] = {
+        {"InitApplication", "()V", (void*) &InitApplication},
+        {"SetListener", "(Latproj/com/linesorter/listener/NativeCallListener;)V", (void*) &SetListener},
+        {"SetFilter", "(Ljava/lang/String;)Z", (void*) &SetFilter},
+        {"AddSourceBlock", "(Ljava/lang/String;)Z", (void*) &AddSourceBlock},
+        {"StartParsing", "()V", (void*) &StartParsing},
+        {"TestParsing", "()I", (void*) &TestParsing},
+};
+
+int jniRegisterNativeMethods(JNIEnv* env, const char* className, const JNINativeMethod* jMethods,
+        int numMethods);
+
+int registerNativeMethods(JNIEnv *env);
+
 JNIEXPORT jint JNICALL
 JNI_OnLoad(JavaVM* vm, void* reserved);
 
-JNIEXPORT void JNICALL
+/*JNIEXPORT void JNICALL
 Java_atproj_com_linesorter_presenters_JNIPresenter_InitApplication(
         JNIEnv *env,
         jobject instance);
@@ -47,7 +68,7 @@ Java_atproj_com_linesorter_presenters_JNIPresenter_StartParsing(
 JNIEXPORT jint JNICALL
 Java_atproj_com_linesorter_presenters_JNIPresenter_TestParsing(
         JNIEnv *env,
-        jobject instance);
+        jobject instance);*/
 
 #ifdef __cplusplus
 };
--- app/src/main/cpp/linesorter_main.cpp
+++ app/src/main/cpp/linesorter_main.cpp
@@ -14,10 +14,103 @@ static const char* kTAG = "linesorter";
 CLogReader *logreader_;
 JniNativeCallListener* listener_;
 
+static void InitApplication(JNIEnv* env, jobject instance) {
+    logreader_ = new CLogReader();
+
+    CLogCallback onProcessCallback;
+    onProcessCallback = [](char *s) {
+        listener_->callJavaOnProcessMethod(s);
+    };
+    logreader_->SetOnProcessCallback(onProcessCallback);
+
+    CLogCallback oncompletecallback;
+    oncompletecallback = [](char *s) {
+        listener_->callJavaOnCompleteMethod();
+    };
+    logreader_->SetOnCompleteCallback(oncompletecallback);
+}
+
+static void SetListener(JNIEnv* env, jobject instance, jobject nativeCallLitener) {
+    listener_ = new JniNativeCallListener(env, nativeCallLitener);
+}
+
+static jboolean SetFilter(JNIEnv *env, jobject instance, jstring jfilter) {
+    const char *filter = env->GetStringUTFChars(jfilter, 0);
+
+    if (!logreader_->SetFilter(filter)) {
+        return JNI_FALSE;
+    }
+
+    return JNI_TRUE;
+}
+
+static jboolean AddSourceBlock(JNIEnv *env, jobject instance, jstring jblock) {
+    const char *block = env->GetStringUTFChars(jblock, 0);
+    size_t block_size = strlen(block);
+
+    if (!logreader_->AddSourceBlock(block, block_size)) {
+        return JNI_FALSE;
+    }
+
+    return JNI_TRUE;
+}
+
+static void StartParsing(JNIEnv *env, jobject instance) {
+    logreader_->Start();
+}
+
+static jint TestParsing(JNIEnv *env, jobject instance) {
+    return logreader_->TestStart();
+}
+
+int jniRegisterNativeMethods(JNIEnv* env, const char* className, const JNINativeMethod* jMethods,
+                             int numMethods) {
+    jclass clazz;
+    int tmp;
+
+    LOGI("Registering %s natives\n", className);
+    clazz = env->FindClass(className);
+    if (clazz == NULL) {
+        LOGI("Native registration unable to find class '%s'\n", className);
+        return -1;
+    }
+    if ((tmp=env->RegisterNatives(clazz, jMethods, numMethods)) <0) {
+        LOGI("RegisterNatives failed for '%s', %d\n", className, tmp);
+        return -1;
+    }
+    return 0;
+}
+
+int registerNativeMethods(JNIEnv *env) {
+    return jniRegisterNativeMethods(env, "atproj/com/linesorter/presenters/JNIPresenter", methods,
+                                    sizeof(methods) / sizeof(methods[0]));
+}
+
 JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env = NULL;
+    jint result = JNI_ERR;
+
+    if (vm->GetEnv((void**) &env, JNI_VERSION_1_6) != JNI_OK) {
+        return result;
+    }
+
+    if (registerNativeMethods(env) != JNI_OK) {
+        return -1;
+    }
+
+
+    result = JNI_VERSION_1_6;
+    LOGI("jni load start: %d", result);
+
+    return result;
+}
+
+/*JNIEXPORT jint JNICALL
 JNI_OnLoad(JavaVM* vm, void* reserved) {
     return JNI_VERSION_1_6;
 }
+
 
 JNIEXPORT void JNICALL
 Java_atproj_com_linesorter_presenters_JNIPresenter_InitApplication(
@@ -87,5 +180,5 @@ Java_atproj_com_linesorter_presenters_JNIPresenter_TestParsing(
         JNIEnv *env,
         jobject instance) {
     return logreader_->TestStart();
-}
+}*/
 
